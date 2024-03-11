4786eace997b4d0717e5a42dbb724ba8c49812d7
kesuoyk <107860025+kesuoyk@users.noreply.github.com>
2024-03-11 20:48:13 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,27 +20,8 @@ alias gcom='git branch --merged | fzf | xargs git checkout'
 alias gcon='git branch --no-merged | fzf | xargs git checkout'
 alias git-delete-merged-branch='git branch --merged | egrep -v "\*" | xargs git branch -d'
 
-# alias ql='qlmanage -p ($argv) >& /dev/null'
-
 function fish_prompt --description 'Write out the prompt'
     printf '%s %s' (set_color green)(prompt_pwd) (set_color yellow)"(*-A-) < "
-end
-
-# function hybrid_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
-#     for mode in default insert visual
-#         fish_default_key_bindings -M $mode
-#     end
-#     fish_vi_key_bindings --no-erase
-# end
-# set -g fish_key_bindings hybrid_bindings
-
-function vagrant -d "Wrapper for vagrant command"
-    switch $argv[1]
-        case 'ssh'
-            ~/bin/vagrant-ssh-with-changing-profile.sh
-        case '*'
-            command vagrant $argv
-     end
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
