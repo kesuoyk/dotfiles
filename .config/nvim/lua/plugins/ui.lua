ce50d2cf58c8040441acbd13d1734716811552f6
kesuoyk <107860025+kesuoyk@users.noreply.github.com>
2026-03-12 23:30:27 +0900

    Add markdown-preview plugin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -100,4 +100,16 @@ return {
       vim.opt.helplang = "ja"
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = "markdown",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    init = function()
+      vim.g.mkdp_auto_close = 0
+      vim.g.mkdp_combine_preview = 1
+    end
+  },
 }
